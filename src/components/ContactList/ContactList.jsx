a080ba09a443cda6d27b313dd43659852ff9e83b
Alexandr <alexandr.kopyletz@gmail.com>
2024-09-15 22:49:37 +0300

    15.09

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectContacts } from '../../redux/contactsSlice';
 import Contact from '../Contact/Contact';
-import contactsData from '../../contacts.json'
+import contactsData from '../../contacts.json';
+import { selectNameFilter } from '../../redux/filtersSlice'
 
 const ContactList = () => {
   const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <ul>
-      {contacts.map(contact => (
+      {filteredContacts.map(contact => (
         <Contact 
           key={contact.id} 
           id={contact.id} 
